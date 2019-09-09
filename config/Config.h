319f2d393f8ab77723f0a9c06c66cbbdfdb28dd0
yuanbin <yuanbin.gu@iris-view.com>
2019-09-09 11:43:50 +0800

    update .gitignore

--- config/Config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-static std::string ROOT_PATH = "/home/gyb/opengl/basic/";
-static std::string SRC_PATH = "/home/gyb/opengl/basic/src/";
-static std::string RES_PATH = "/home/gyb/opengl/basic/resource/";
